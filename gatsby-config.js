96bda21e8160e1b9a267d0cde8dfe421b5c4ec79
El Mehdi Essaadi <gsio1487@gmail.com>
2022-09-14 23:36:18 +0100

    add google analytics by gatsby-plugin-google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,5 +46,19 @@ module.exports = {
             icon: "src/assets/images/logo.png",
          },
       },
+      {
+         resolve: `gatsby-plugin-google-gtag`,
+         options: {
+            // You can add multiple tracking ids and a pageview event will be fired for all of them.
+            trackingIds: [
+               "G-QTHZYEYQSS", // Google Analytics 
+            ],
+            // This object is used for configuration specific to this plugin
+            pluginConfig: {
+               // Puts tracking script in the head instead of the body
+               head: true,
+            },
+         },
+      },
    ],
 };
