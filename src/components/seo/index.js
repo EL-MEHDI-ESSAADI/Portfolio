00e1592140b5ce90dde1e634bf03a8cdfdc6a10e
EL MEHDI ESSAADI <gsio1487@gmail.com>
2022-09-14 22:37:31 +0100

    fix google tag semicolon error

--- src/components/seo/index.js
+++ src/components/seo/index.js
@@ -57,7 +57,7 @@ function Seo() {
          <script async src="https://www.googletagmanager.com/gtag/js?id=G-QTHZYEYQSS"></script>
          <script>
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
+            function gtag(){dataLayer.push(arguments)}
             gtag('js', new Date());
 
             gtag('config', 'G-QTHZYEYQSS');
