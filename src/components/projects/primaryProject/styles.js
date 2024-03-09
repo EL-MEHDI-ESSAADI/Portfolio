735e4950d8a477a347f7f934b31b404617d92bdd
EL MEHDI ESSAADI <gsio1487@gmail.com>
2024-03-09 12:54:57 +0000

    - add quran mobile app project

--- src/components/projects/primaryProject/styles.js
+++ src/components/projects/primaryProject/styles.js
@@ -1,84 +1,84 @@
 import styled from "styled-components";
 
 export const StyledLink = styled.a`
-   --link-bg-color: var(--cl-primary-300);
-   position: relative;
-   padding: 0.47rem 1.1rem;
-   font-weight: 700;
-   color: var(--cl-white);
-   background-color: var(--link-bg-color);
-   user-select: none;
-   transform: rotate(-2deg);
+  --link-bg-color: var(--cl-primary-300);
+  position: relative;
+  padding: 0.47rem 1.1rem;
+  font-weight: 700;
+  color: var(--cl-white);
+  background-color: var(--link-bg-color);
+  user-select: none;
+  transform: rotate(-2deg);
 
-   &::after {
-      content: "";
-      position: absolute;
-      inset: 0;
-      z-index: -1;
-      border: 1.5px solid var(--link-bg-color);
-      transform: translate(0.3rem, -0.3rem);
-      transition: var(--trn-fast-smooth-all);
-   }
-   &:hover::after,
-   &:focus-visible::after {
-      transform: translate(0.2rem, -0.2rem);
-   }
+  &::after {
+    content: "";
+    position: absolute;
+    inset: 0;
+    z-index: -1;
+    border: 1.5px solid var(--link-bg-color);
+    transform: translate(0.3rem, -0.3rem);
+    transition: var(--trn-fast-smooth-all);
+  }
+  &:hover::after,
+  &:focus-visible::after {
+    transform: translate(0.2rem, -0.2rem);
+  }
 `;
 
 export const InfoContainer = styled.div`
-   flex: 1;
+  flex: 1;
 `;
 
 export const ImgContainer = styled.div`
-   order: -1;
-   align-self: center;
-   width: min(100%, 500px);
-   box-shadow: ${({ isDarkModeEnabled }) =>
-      isDarkModeEnabled
-         ? "0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 2px 1px -1px rgb(0 0 0 / 12%), 0px 1px 3px 0px rgb(0 0 0 / 20%)"
-         : "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"};
+  order: -1;
+  align-self: center;
+  width: min(100%, 500px);
+  box-shadow: ${({ isDarkModeEnabled }) =>
+    isDarkModeEnabled
+      ? "0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 2px 1px -1px rgb(0 0 0 / 12%), 0px 1px 3px 0px rgb(0 0 0 / 20%)"
+      : "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"};
 
-   @media (min-width: 768px) {
-      width: clamp(21.875rem, 45.5vw, 31.25rem);
-      order: 1;
-   }
+  @media (min-width: 768px) {
+    width: clamp(21.875rem, 45.5vw, 31.25rem);
+    order: 1;
+  }
 
-   img {
-      width: auto;
-      height: auto;
-   }
+  img {
+    width: auto;
+    height: auto;
+  }
 `;
 
 export const Paragraph = styled.p`
-   text-align: justify;
+  text-align: justify;
 
-   a {
-      text-decoration: underline;
-      transition: var(--trn-fast-smooth-color);
-      &:hover,
-      &:focus {
-         color: var(--cl-accent);
-      }
-   }
+  a {
+    text-decoration: underline;
+    transition: var(--trn-fast-smooth-color);
+    &:hover,
+    &:focus {
+      color: var(--cl-accent);
+    }
+  }
 `;
 
 export const StyledProject = styled.article`
-   display: flex;
-   flex-direction: column;
-   justify-content: space-between;
-   gap: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 2rem;
 
-   &:nth-of-type(n + 2) ${ImgContainer} {
-      @media (min-width: 768px) {
-         order: -1;
-      }
-   }
+  &:nth-of-type(even) ${ImgContainer} {
+    @media (min-width: 768px) {
+      order: -1;
+    }
+  }
 
-   &:not(:first-child) {
-      margin-top: 3rem;
-   }
+  &:not(:first-child) {
+    margin-top: 3rem;
+  }
 
-   @media (min-width: 768px) {
-      flex-direction: row;
-   }
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
--- src/data/index.js
+++ src/data/index.js
@@ -3,159 +3,189 @@ import { FaLightbulb, FaLinkedinIn, FaGithubAlt } from "react-icons/fa";
 import { GiBrain } from "react-icons/gi";
 
 export const landingPageSections = new Map([
-   [
-      "about",
-      {
-         link: "#about",
-         Icon: BsFillPersonFill,
-      },
-   ],
-   [
-      "projects",
-      {
-         link: "#projects",
-         Icon: FaLightbulb,
-      },
-   ],
-   [
-      "skills",
-      {
-         link: "#skills",
-         Icon: GiBrain,
-      },
-   ],
+  [
+    "about",
+    {
+      link: "#about",
+      Icon: BsFillPersonFill,
+    },
+  ],
+  [
+    "projects",
+    {
+      link: "#projects",
+      Icon: FaLightbulb,
+    },
+  ],
+  [
+    "skills",
+    {
+      link: "#skills",
+      Icon: GiBrain,
+    },
+  ],
 ]);
 
 export const mySocialMedias = [
-   ["linkedin", { link: "https://www.linkedin.com/in/el-mehdi-essaadi", Icon: FaLinkedinIn }],
-   ["github", { link: "https://github.com/EL-MEHDI-ESSAADI", Icon: FaGithubAlt }],
-   ["twitter", { link: "https://twitter.com/Von__Mehdi", Icon: BsTwitter }],
+  [
+    "linkedin",
+    {
+      link: "https://www.linkedin.com/in/el-mehdi-essaadi",
+      Icon: FaLinkedinIn,
+    },
+  ],
+  [
+    "github",
+    { link: "https://github.com/EL-MEHDI-ESSAADI", Icon: FaGithubAlt },
+  ],
+  ["twitter", { link: "https://twitter.com/Von__Mehdi", Icon: BsTwitter }],
 ];
 
 export const myEmailLink = "mehdiessaadion@gmail.com";
 
 export const projectsData = {
-   primary: [
-      {
-         name: "Voneet",
-         descriptiveName: "Twitter Tool",
-         projectSectionId: "twitter-tool",
-         description: `Voneet is a Twitter tool that has some new Twitter features, like configuring welcome messages and 
+  primary: [
+    {
+      name: "Voneet",
+      descriptiveName: "Twitter Tool",
+      projectSectionId: "twitter-tool",
+      description: `Voneet is a Twitter tool that has some new Twitter features, like configuring welcome messages and 
          searching for spaces. I 
          <a target="_blank" rel="noreferrer" href="https://devpost.com/software/voneet">participated</a>
          with it in the 
          <a target="_blank" rel="noreferrer" href="https://chirpdevchallenge.devpost.com">Chirp challenge</a>.`,
-         usedTechnologies:
-            "Twitter Api - React - NodeJS - Express - MonogoDB - Styled Componets - Bootstrap - React Bootstrap - React Testing Library",
-         demoLink: "https://voneet.mehdiessaadi.com",
-         githubRepoLink: "https://github.com/EL-MEHDI-ESSAADI/Voneet",
-         imgName: "voneetProject",
-         imgWidth: "1115",
-         imgHight: "689",
-         showAsLinkInAboutSection: true,
-         id: 0,
-      },
-      {
-         name: "Quran For You",
-         descriptiveName: "Quran Player",
-         projectSectionId: "quran-player",
-         description:
-            "Quran For You is a web app for reading and listening to Qur'an, the app has several translations and reciters.",
-         usedTechnologies: "React js - React Router Dom - Scss",
-         demoLink: "https://quran.mehdiessaadi.com",
-         githubRepoLink: "https://github.com/EL-MEHDI-ESSAADI/Quran-Player",
-         imgName: "quranProject",
-         imgWidth: "1351",
-         imgHight: "683",
-         showAsLinkInAboutSection: true,
-         id: 1,
-      },
-   ],
-   secondary: [
-      {
-         name: "Get Recipe",
-         description: "A web app that lets you search for your desired recipe by the meal's name.",
-         projectSectionId: "get-recipe",
-         usedTechnologies: "React Js - Scss",
-         demoLink: "https://get-recipe-now.netlify.app",
-         githubRepoLink: "https://github.com/EL-MEHDI-ESSAADI/Recipe_App",
-         id: 2,
-      },
-      {
-         name: "Coffee Shop",
-         descriptiveName: "Coffee Shop",
-         projectSectionId: "coffee-shop",
-         description: "A landing page for a coffee shop",
-         usedTechnologies: "Vanilla Js - Scss",
-         demoLink: "https://coffee.mehdiessaadi.com",
-         githubRepoLink: "https://github.com/EL-MEHDI-ESSAADI/Coffee-Shop",
-         showAsLinkInAboutSection: true,
-         id: 3,
-      },
-      {
-         name: "SUBLIME",
-         description: "A dark theme template designed for creative agencies.",
-         projectSectionId: "sublime",
-         usedTechnologies: "Vanilla Js - Scss",
-         demoLink: "https://sublime.mehdiessaadi.com",
-         githubRepoLink: "https://github.com/EL-MEHDI-ESSAADI/SUBLIME-TEMPLATE",
-         id: 4,
-      },
-   ],
+      usedTechnologies:
+        "Twitter Api - React - NodeJS - Express - MonogoDB - Styled Componets - Bootstrap - React Bootstrap - React Testing Library",
+      demoLink: "https://voneet.mehdiessaadi.com",
+      githubRepoLink: "https://github.com/EL-MEHDI-ESSAADI/Voneet",
+      imgName: "voneetProject",
+      imgWidth: "1115",
+      imgHight: "689",
+      showAsLinkInAboutSection: true,
+      id: 0,
+    },
+    {
+      name: "Quran For You",
+      descriptiveName: "Quran Player",
+      projectSectionId: "quran-player",
+      description:
+        "Quran For You is a web app for reading and listening to Qur'an, the app has several translations and reciters.",
+      usedTechnologies: "React js - React Router Dom - Scss",
+      demoLink: "https://quran.mehdiessaadi.com",
+      githubRepoLink: "https://github.com/EL-MEHDI-ESSAADI/Quran-Player",
+      imgName: "quranProject",
+      imgWidth: "1351",
+      imgHight: "683",
+      showAsLinkInAboutSection: true,
+      id: 1,
+    },
+    {
+      name: "Quran",
+      descriptiveName: "Quran Mobile App",
+      projectSectionId: "quran-mobile",
+      description:
+        "Quran is a mobile app for reading the Qur'an with multiple translations. Listening feature with diverse reciters coming soon.",
+      usedTechnologies: "React Native, Expo, Tailwid",
+      demoLink:
+        "https://github.com/EL-MEHDI-ESSAADI/mobile-quran-app/assets/79756353/a1039945-b8f5-4cd9-8fe2-1a8ce4a2be5a",
+      githubRepoLink: "https://github.com/EL-MEHDI-ESSAADI/mobile-quran-app",
+      imgName: "quranMobile",
+      imgWidth: "1351",
+      imgHight: "683",
+      showAsLinkInAboutSection: true,
+      id: 2,
+    },
+  ],
+  secondary: [
+    {
+      name: "Get Recipe",
+      description:
+        "A web app that lets you search for your desired recipe by the meal's name.",
+      projectSectionId: "get-recipe",
+      usedTechnologies: "React Js - Scss",
+      demoLink: "https://get-recipe-now.netlify.app",
+      githubRepoLink: "https://github.com/EL-MEHDI-ESSAADI/Recipe_App",
+      id: 2,
+    },
+    {
+      name: "Coffee Shop",
+      descriptiveName: "Coffee Shop",
+      projectSectionId: "coffee-shop",
+      description: "A landing page for a coffee shop",
+      usedTechnologies: "Vanilla Js - Scss",
+      demoLink: "https://coffee.mehdiessaadi.com",
+      githubRepoLink: "https://github.com/EL-MEHDI-ESSAADI/Coffee-Shop",
+      showAsLinkInAboutSection: true,
+      id: 3,
+    },
+    {
+      name: "SUBLIME",
+      description: "A dark theme template designed for creative agencies.",
+      projectSectionId: "sublime",
+      usedTechnologies: "Vanilla Js - Scss",
+      demoLink: "https://sublime.mehdiessaadi.com",
+      githubRepoLink: "https://github.com/EL-MEHDI-ESSAADI/SUBLIME-TEMPLATE",
+      id: 4,
+    },
+  ],
 };
 
 export const mentionedProjectsInAbout = [
-   ...projectsData.primary.filter(({ showAsLinkInAboutSection }) => showAsLinkInAboutSection),
-   ...projectsData.secondary.filter(({ showAsLinkInAboutSection }) => showAsLinkInAboutSection),
+  ...projectsData.primary.filter(
+    ({ showAsLinkInAboutSection }) => showAsLinkInAboutSection
+  ),
+  ...projectsData.secondary.filter(
+    ({ showAsLinkInAboutSection }) => showAsLinkInAboutSection
+  ),
 ];
 
 export const mySkills = [
-   {
-      name: "HTML5",
-      imgName: "html",
-   },
-   {
-      name: "CSS3",
-      imgName: "css",
-   },
-   {
-      name: "Sass",
-      imgName: "sass",
-   },
-   {
-      name: "JavaScript",
-      imgName: "javascript",
-   },
-   {
-      name: "TypeScript",
-      imgName: "typescript",
-   },
-   {
-      name: "React JS",
-      imgName: "react",
-   }
-   ,{
-      name: "Next JS",
-      imgName: "next",
-   }
-   ,{
-      name: "React Native",
-      imgName: "react",
-   }
-   ,{
-      name: "Tailwind",
-      imgName: "tailwind",
-   },
-   {
-      name: "Bootstrap",
-      imgName: "bootstrap",
-   },
-   {
-      name: "Styled Components",
-      imgName: "styled-components",
-   },
-   {
-      name: "Git",
-      imgName: "git",
-   },
-]
+  {
+    name: "HTML5",
+    imgName: "html",
+  },
+  {
+    name: "CSS3",
+    imgName: "css",
+  },
+  {
+    name: "Sass",
+    imgName: "sass",
+  },
+  {
+    name: "JavaScript",
+    imgName: "javascript",
+  },
+  {
+    name: "TypeScript",
+    imgName: "typescript",
+  },
+  {
+    name: "React JS",
+    imgName: "react",
+  },
+  {
+    name: "Next JS",
+    imgName: "next",
+  },
+  {
+    name: "React Native",
+    imgName: "react",
+  },
+  {
+    name: "Tailwind",
+    imgName: "tailwind",
+  },
+  {
+    name: "Bootstrap",
+    imgName: "bootstrap",
+  },
+  {
+    name: "Styled Components",
+    imgName: "styled-components",
+  },
+  {
+    name: "Git",
+    imgName: "git",
+  },
+];
